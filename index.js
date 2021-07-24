d3386d233189c85f52dadfc8cc3068e466f41199
Summit <67283504+SummitStudiosDev@users.noreply.github.com>
2021-07-24 22:37:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express();
+var bodyParser = require('body-parser')
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.use('/static', express.static('public'))
+
+
+app.get('/', function(req, res) {
+   res.render("index.html")
+});
+
+
+let port = 8080;
+app.listen(port, () => {
+  console.log(`Website listening at http://localhost:${port}`)
+});
