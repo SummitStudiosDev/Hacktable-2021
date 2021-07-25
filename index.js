44dd48b5963f6ae359eefe551fdbafe8c95b1509
Summit <67283504+SummitStudiosDev@users.noreply.github.com>
2021-07-25 18:49:29 +0000

    10 more minutes to deadline, this should be the final release
    :)

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get('/mybadges', function(req, res) {
 });
 
 app.get('/games', function(req, res) {
-   res.send("Unfortunately, due to technical difficulties, we could not embed the game onto the website. If you would like to test it, here is the download link: <a href='https://flying-in.space/PZ1JMTgmR2.zip?key=AcdM7aNqyV2s81'> Download Link </a> <br> <br> In the game, you collect food, and the goal is to collect all the food!")
+   res.render("games.html")
 });
 
 
